6dd87efd29aa5504fbd9acb474a686d605f2f694
Akmaliyah11 <akmaliyah0518@gmail.com>
2024-11-04 17:25:14 +0700

    Add files via upload

---
+++ SI_RegPagi23176007Latihan18Kelipatan.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package si_regpagi.pkg23176007.latihan18.kelipatan;
+
+/**
+ *
+ * @author 
+ * Nama              : Akmaliyah
+ * NIM               : 23176007
+ * Kelas             : PBO12
+ * Prodi             : Sistem Informasi
+ * Deskripsi Program : Program ini berisi program untuk menampilkan Kelipatan
+ */
+
+public class SI_RegPagi23176007Latihan18Kelipatan {
+
+    public static void main(String[] args) {
+        
+        double angka = 3.5;
+        while (angka <= 35){
+            System.out.println(angka);
+            angka += 3.5;
+        }
+    }
+    
+}
